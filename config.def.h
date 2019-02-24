e7c2ab251217d39514bb917778b7316fc54685aa
ichingking <ichingking@yandex.com>
2019-02-24 19:54:19 +0800

    change symbol and key

--- config.def.h
+++ config.def.h
@@ -35,9 +35,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[D]",      tile },    /* first entry is default */
+	{ "[⚋]",      monocle },    /* first entry is default */
 	{ "[N]",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[⚊]",      tile },
 };
 
 /* key definitions */
@@ -75,9 +75,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
